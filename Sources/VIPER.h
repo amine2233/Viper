8ecde660cc9824f63e715bab6ac5a3b8a90f3782
Bensalah <amine.bensalah@outlook.com>
2018-03-21 08:25:51 +0100

    first commit

---
+++ Sources/VIPER.h
@@ -0,0 +1,16 @@
+//
+//  VIPER.h
+//  VIPER
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VIPER.
+FOUNDATION_EXPORT double VIPER_VersionNumber;
+
+//! Project version string for VIPER.
+FOUNDATION_EXPORT const unsigned char VIPER_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VIPER/PublicHeader.h>
+
+
